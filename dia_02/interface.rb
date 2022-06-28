c907161de1c79a7e93770f33b223852860d78a9e
tvbgo <tomas.gomes@rdstation.com>
2022-06-28 10:27:48 -0300

    corrected interface issue

--- dia_02/interface.rb
+++ dia_02/interface.rb
@@ -1,14 +1,15 @@
-require_relative './desafios_02.rb'
+require_relative './desafios.rb'
+
 
 def welcome_message
-	puts "-------------------------------------------"
-	puts "Bem-vinda a interface de Ruby para o seu BD"
-	p "-------------------------------------------"
-	puts "1 - Create Artist"
-	puts "2 - Find Artist"
-	puts "3 - Find All Artists"
-	puts "4 - Update Artist"
-	puts "5 - Delete Artist"
+  puts "-------------------------------------------"
+  puts "Bem-vinda a interface de Ruby para o seu BD"
+  p "-------------------------------------------"
+  puts "1 - Create Artist"
+  puts "2 - Find Artist"
+  puts "3 - Find All Artists"
+  puts "4 - Update Artist"
+  puts "5 - Delete Artist"
 end
 
 def actions
@@ -17,56 +18,55 @@ def actions
     case @input
       when 1
         puts "---------------------------------------"
-        puts "Insira o nome da(o) artista"
+        puts "Insira o nome"
         @name = gets.chomp
-        if Artist.new(@name).save
-        	puts "Salvo com sucesso"
+        artist = Artist.new(name: @name)
+        if Artist.save(artist)
+          puts "Salvo com sucesso"
         else
-        	puts "Não funcionou..."
+          puts "Não funcionou..."
         end
       when 2
         puts "---------------------------------------"
         puts "Insira o ID"
         @id = gets.chomp.to_i
         artist = Artist.find(@id)
+        p artist
         if artist
-        	puts "Parabens, encontrei: #{artist.name}"
+          puts "Parabens, encontrei: #{artist.name}"
         else
-        	puts "Não funcionou, ou não há ninguém com esse ID"
+          puts "Não funcionou, ou não há ninguém com esse ID"
         end
       when 3
         puts "---------------------------------------"
         puts "Tentando buscar todos os artists"
         artists = Artist.all
         if !artists.empty?
-        	puts "Parabens, encontrei: #{artists.length} registros"
+          puts "Parabens, encontrei: #{artists.length} registros"
         else
-        	puts "Não funcionou......"
+          puts "Não funcionou......"
         end
       when 4
         puts "---------------------------------------"
         puts "Qual o ID para alterar?"
         @id = gets.chomp.to_i
         artist = Artist.find(@id)
-        puts artist
         puts "Qual o novo nome?"
-        @nome = gets.chomp
-        if Artist.new(@id, @name).save
-        	puts "Atualizado com sucesso"
+        @name = gets.chomp
+        artist.name = @name
+        if Artist.save(artist)
+          puts "Atualizado com sucesso"
         else
-        	puts "Não funcionou..."
+          puts "Não funcionou..."
         end
       when 5
-      	puts "---------------------------------------"
+        puts "---------------------------------------"
         puts "Qual o ID para deletar?"
         @id = gets.chomp.to_i
-        artist = Artist.find(@id)
-        puts "Qual o novo nome?"
-        @nome = gets.chomp
-        if Artist.new(@id, @name).destroy
-        	puts "Removido com sucesso"
+        if Artist.destroy(@id)
+          puts "Removido com sucesso"
         else
-        	puts "Não funcionou..."
+          puts "Não funcionou..."
         end
       else
         puts "---------------------------------------"
